96713d3cb2728bbc80210462e12cfcfcf43ad53f
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-02-23 17:21:16 +0800

    add getWinner function. Still haven't cracked validation of playerMove input.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,37 +21,74 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    var move = getInput().toLowerCase();
-    // If a `move` has a value, your expression should evaluate to that value.
-    if (typeof move != "string") {
-    	console.log("That is not a valid answer. Try again.");
-    	move = getInput();
-    }
-    else if (move != ("rock" || "paper" || "scissors")) {
-    	console.log("That is not a valid option. Try again.");
-    	move = getInput();
-    }
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+function getPlayerMove() {
+    var playerMove = getInput();
+        switch(playerMove) {
+            case "rock":
+                console.log("You chose rock!");
+                break;
+            case "paper":
+                console.log("You chose paper!");
+                break;
+            case "scissors":
+                console.log("You chose scissors!");
+                break;
+            default:
+                console.log("That is not a valid choice. Please choose again.");
+        }
+    return playerMove;
 }
+
+/*    // Write an expression that operates on a variable called `move`
+    var playerMove = getInput();
+    // If a `move` has a value, your expression should evaluate to that value.
+    var validChoices = ["rock", "paper", "scissors"]
+    
+    for (i = 0; i < validChoices.length; i++) {
+        while(playerMove !== validChoices[i]) {
+            console.log("Invalid choice...try again!");
+            playerMove = getInput();
+        }
+        console.log("You chose " + playerMove);
+        return playerMove;
+    }*/
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
+    var compMove = randomPlay();
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return compMove;
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+function getWinner(playerMove, compMove) {
+    if (playerMove === compMove) 
+    {
+    return ("The result is a tie!");
+    }
+    else if (playerMove === "rock") 
+    {
+        if (compMove === "scissors")
+            return ("rock wins");
+        else
+            return ("paper wins");
+    }
+    else if (playerMove === "paper") 
+    {
+        if (compMove === "rock")
+            return ("paper wins");
+        else
+            return ("scissors wins");
+    }
+    else if (playerMove === "scissors") 
+    {
+        if (compMove === "rock")
+            return ("rock wins");
+    else 
+            return ("scissors wins");
+    }
 }
+
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
